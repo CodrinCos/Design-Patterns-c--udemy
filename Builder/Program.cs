418e1f2177c096778ff63c61d989268bea46742e
CodrinCos <codrin.costea22@gmail.com>
2022-08-29 10:27:03 +0200

    Inheritance for deep copy

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,60 +1,113 @@
-﻿
-var john = new Employee("John", new Address("123 London Road", "London", "UK"));
+﻿var john = new Employee();
+john.Names = new[] { "John", "Doe" };
+john.Address = new Address { HouseNumber = 123, StreetName = "London Road" };
+john.Salary = 321000;
+var copy = john.DeepCopy();
 
-//var chris = john;
-var chris = new Employee(john);
+copy.Names[1] = "Smith";
+copy.Address.HouseNumber++;
+copy.Salary = 123000;
 
-chris.Name = "Chris";
-Console.WriteLine(john); // oops, john is called chris
-Console.WriteLine(chris);
+Console.WriteLine(john);
+Console.WriteLine(copy);
 
-public class Address
+public interface IDeepCopyable<T> where T : new()
 {
-    public string StreetAddress, City, Country;
+    void CopyTo(T target);
 
-    public Address(string streetAddress, string city, string country)
+    public T DeepCopy()
     {
-        StreetAddress = streetAddress ?? throw new ArgumentNullException(paramName: nameof(streetAddress));
-        City = city ?? throw new ArgumentNullException(paramName: nameof(city));
-        Country = country ?? throw new ArgumentNullException(paramName: nameof(country));
+        T t = new T();
+        CopyTo(t);
+        return t;
+    }
+}
+
+public class Address : IDeepCopyable<Address>
+{
+    public string StreetName;
+    public int HouseNumber;
+
+    public Address(string streetName, int houseNumber)
+    {
+        StreetName = streetName;
+        HouseNumber = houseNumber;
     }
 
-    public Address(Address other)
+    public Address()
     {
-        StreetAddress = other.StreetAddress;
-        City = other.City;
-        Country = other.Country;
+
     }
 
     public override string ToString()
     {
-        return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(City)}: {City}, {nameof(Country)}: {Country}";
+        return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+    }
+
+    public void CopyTo(Address target)
+    {
+        target.StreetName = StreetName;
+        target.HouseNumber = HouseNumber;
     }
 }
 
-public class Employee
+
+
+public class Person : IDeepCopyable<Person>
 {
-    public string Name;
+    public string[] Names;
     public Address Address;
 
-    public Employee(string name, Address address)
+    public Person()
     {
-        Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
-        Address = address ?? throw new ArgumentNullException(paramName: nameof(address));
+
     }
 
-    public Employee(Employee other)
+    public Person(string[] names, Address address)
     {
-        Name = other.Name;
-        Address = new Address(other.Address);
+        Names = names;
+        Address = address;
     }
 
     public override string ToString()
     {
-        return $"{nameof(Name)}: {Name}, {nameof(Address)}: {Address}";
+        return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+    }
+
+    public virtual void CopyTo(Person target)
+    {
+        target.Names = (string[])Names.Clone();
+        target.Address = Address.DeepCopy();
     }
 }
 
+public class Employee : Person, IDeepCopyable<Employee>
+{
+    public int Salary;
 
+    public void CopyTo(Employee target)
+    {
+        base.CopyTo(target);
+        target.Salary = Salary;
+    }
 
+    public override string ToString()
+    {
+        return $"{base.ToString()}, {nameof(Salary)}: {Salary}";
+    }
+}
 
+public static class DeepCopyExtensions
+{
+    public static T DeepCopy<T>(this IDeepCopyable<T> item)
+      where T : new()
+    {
+        return item.DeepCopy();
+    }
+
+    public static T DeepCopy<T>(this T person)
+      where T : Person, new()
+    {
+        return ((IDeepCopyable<T>)person).DeepCopy();
+    }
+}
