648e9ba2c27c1210e3d3b85cc3897a712e5c4fc8
CodrinCos <codrin.costea22@gmail.com>
2022-08-31 08:38:15 +0200

    Described the issues when using singleton

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using MoreLinq;
+using NUnit.Framework;
 
 var db = SingletonDatabase.Instance;
 var city = "Tokyo";
@@ -16,12 +17,15 @@ public interface IDatabase
 public class SingletonDatabase : IDatabase
 {
     private Dictionary<string, int> capitals;
+    private static int instanceCount;
+    public static int Count => instanceCount;
 
     private SingletonDatabase()
     {
+        instanceCount++;
         Console.WriteLine("init db");
 
-        capitals = File.ReadAllLines("Capitals.txt")
+        capitals = File.ReadAllLines(Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName!, "Capitals.txt"))
                     .Batch(2)
                     .ToDictionary(
                          list => list.ElementAt(0).Trim(), 
@@ -36,4 +40,47 @@ public class SingletonDatabase : IDatabase
     private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
     public static SingletonDatabase Instance => instance.Value;
+}
+
+public class SingletonRecordFinder
+{
+    public int GetTotalPopulation(IEnumerable<string> names)
+    {
+        int result = 0;
+        foreach(var name in names)
+        {
+            result += SingletonDatabase.Instance.GetPopulation(name);
+        }
+        return result;
+    }
+}
+
+[TestFixture]
+public class SingletonTests
+{
+    [Test]
+    public void IsSingletonTest()
+    {
+        var db = SingletonDatabase.Instance;
+        var db2 = SingletonDatabase.Instance;
+
+        Assert.That(db, Is.SameAs(db2));
+        Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void SingletonTotalPopulationTest()
+    {
+        //This test makes dependent on the real db, in case its costly then it might be a problem.
+        //In case a city is removed then it might be a problem.
+        //And!! SingletonRecordFinder has a hardcoded record to the instance!!
+        //Once you start using it you hard code the reference everywhere!!
+        var rf = new SingletonRecordFinder();
+
+        var names = new[] { "Seoul", "Mexico City" };
+
+        int tp = rf.GetTotalPopulation(names); 
+
+        Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+    }
 }
