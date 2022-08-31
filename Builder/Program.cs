3d4295295caa776d78839d27985fb3ba7f3af16a
CodrinCos <codrin.costea22@gmail.com>
2022-08-31 09:22:56 +0200

    addign the singleton in dependency injection, a way to test singleton

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using Autofac;
 using MoreLinq;
 using NUnit.Framework;
 
@@ -55,6 +56,62 @@ public class SingletonRecordFinder
     }
 }
 
+public class OrdinaryDatabase : IDatabase
+{
+    private Dictionary<string, int> capitals;
+
+    private OrdinaryDatabase()
+    {
+        Console.WriteLine("init db");
+
+        capitals = File.ReadAllLines(Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName!, "Capitals.txt"))
+                    .Batch(2)
+                    .ToDictionary(
+                         list => list.ElementAt(0).Trim(),
+                         list => int.Parse(list.ElementAt(1))
+                     );
+    }
+
+    public int GetPopulation(string name)
+    {
+        return capitals[name];
+    }
+}
+
+    //Feed with other db
+public class ConfigurableRecordFinder
+{
+    private IDatabase database;
+
+    public ConfigurableRecordFinder(IDatabase database)
+    {
+        this.database = database ?? throw new ArgumentNullException(paramName: nameof(database));
+    }
+
+    public int GetTotalPopulation(IEnumerable<string> names)
+    {
+        int result = 0;
+        foreach (var name in names)
+        {
+            result += database.GetPopulation(name);
+        }
+        return result;
+    }
+}
+
+public class DummyDatabase : IDatabase
+{
+    public int GetPopulation(string name)
+    {
+        return new Dictionary<string, int>
+        {
+            ["alpha"] = 1,
+            ["beta"] = 2,
+            ["gamma"] = 3
+        }[name];
+    }
+}
+
 [TestFixture]
 public class SingletonTests
 {
@@ -83,4 +140,32 @@ public class SingletonTests
 
         Assert.That(tp, Is.EqualTo(17500000 + 17400000));
     }
+
+    [Test]
+    public void ConfigurablePopulationTest()
+    {
+        var rf = new ConfigurableRecordFinder(new DummyDatabase());
+        var names = new[] { "alpha", "gamma" };
+
+        int tp = rf.GetTotalPopulation(names);
+
+        Assert.That(tp, Is.EqualTo(4));
+    }
+
+    [Test]
+    public void DIPopulationTest()
+    {
+        //Container builder => Autofac
+        var cb = new ContainerBuilder();
+
+        cb.RegisterType<OrdinaryDatabase>().As<IDatabase>().SingleInstance();
+
+        cb.RegisterType<ConfigurableRecordFinder>();
+
+        using(var c = cb.Build())
+        {
+            var rf = c.Resolve<ConfigurableRecordFinder>();
+        }
+    }
 }
+
