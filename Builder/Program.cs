fb6ae3ead9e63a4f93c87669cafd1daccb2e916e
CodrinCos <codrin.costea22@gmail.com>
2022-09-02 08:29:20 +0200

    this is singleton only if you do it per thread

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,28 +1,102 @@
-﻿
-var t1 = Task.Factory.StartNew(() => {
-	Console.WriteLine("t1: " + PerThreadSingleton.Instance.Id);
-});
+﻿//
 
-var t2 = Task.Factory.StartNew(() => {
-    Console.WriteLine("t2: " + PerThreadSingleton.Instance.Id);
-});
+using System.Text;
 
-Task.WaitAll(t1, t2);
+var house = new Builduing();
 
-//one class per thread
-public sealed class PerThreadSingleton
+//ground floor 300
+using(new BuildingContext(3000))
 {
-	private static ThreadLocal<PerThreadSingleton> threadInstance =
-		new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+    house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+    house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
 
-	public int Id;
-	
-	private PerThreadSingleton()
-	{
-		Id = Thread.CurrentThread.ManagedThreadId;
-	}
 
-	public static PerThreadSingleton Instance => threadInstance.Value;
+    // 1st floor 3500
+    using (new BuildingContext(3500))
+    {
+        house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+        house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
+    }
 
+    //2nd floor 3000
+    house.Walls.Add(new Wall(new Point(0, 0), new Point(5000, 0)));
+    house.Walls.Add(new Wall(new Point(0, 0), new Point(0, 4000)));
 }
 
+Console.WriteLine(house.ToString());    
+
+//height is an ambient context
+
+public sealed class BuildingContext : IDisposable // - ambient context - not thread safe - find solution for this.
+{
+    public int WallHeight;
+    private static Stack<BuildingContext> stack = new Stack<BuildingContext>();
+
+    static BuildingContext()
+    {
+        stack.Push(new BuildingContext(0));
+    }
+
+    public BuildingContext(int wallHeight)
+    {
+        WallHeight = wallHeight;
+        stack.Push(this);
+
+    }
+
+    public static BuildingContext Current => stack.Peek();
+
+    public void Dispose()
+    {
+        if (stack.Count > 1)
+            stack.Pop();
+    }
+}
+
+public class Builduing
+{
+    public List<Wall> Walls = new List<Wall>();
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        foreach(var wall in Walls)
+        {
+            sb.Append(wall.ToString());
+        }
+        return sb.ToString();
+    }
+}
+
+public class Point
+{
+    private int x, y;
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(x)}: {x}, {nameof(y)}: {y}";    
+    }
+}
+
+public class Wall
+{
+    public Point Start, End;
+    public int Height;
+
+    public Wall(Point start, Point end)
+    {
+        Start = start;
+        End = end;
+        this.Height = BuildingContext.Current.WallHeight;
+    }
+
+    public override string ToString()
+    {
+        return $"{nameof(Start)}: {Start}, {nameof(End)}: {End}, {nameof(Height)}: {Height}";
+    }
+}
