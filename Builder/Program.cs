a52f27820898a4767243959ef3ebca1baafa86fc
CodrinCos <codrin.costea22@gmail.com>
2022-09-26 08:41:58 +0200

    generic value adapters + overwrite + and others

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,172 +1,143 @@
-﻿using MoreLinq;
-using System.Collections;
-using System.Collections.ObjectModel;
+﻿//Generic value adapter
+//implement vector, 2 dimen, 3 dimen (int floating etc...)
+//generic approach to implement all of these
+//result => Vector2f, Vector3i, ...
 
-List<VectorObject> vectorObjects = new List<VectorObject>
+//adapt a literal value to a type
+
+var v = new Vector2i(1, 2);
+
+v[0] = 0;
+
+var vv = new Vector2i(2, 3);
+
+//I want to do the following:
+var result = v + vv;
+//atm I cannot do it in Vector class because its a generic type
+
+
+
+public interface IInteger
 {
-      new VectorRectangle(1, 1, 10, 10),
-      new VectorRectangle(3, 3, 6, 6)
-};
-
-Draw(vectorObjects);
-Draw(vectorObjects);
-
-static void Draw(List<VectorObject> vectorObjects)
-{
-    foreach (var vo in vectorObjects)
+    int Value { get; }
+}
+public static class Dimensions
+{ 
+    public class Two : IInteger
     {
-        foreach (var line in vo)
+        public int Value => 2;
+    }
+
+    public class Three : IInteger
+    {
+        public int Value => 3;
+    }
+}
+
+public class VectorOfFloat<TSelf, D> : 
+    Vector<TSelf, float, D>
+    where D : IInteger, new()
+    where TSelf : Vector<TSelf, float, D>, new()
+{
+
+}
+
+public class VectorOfInt<D> : Vector<VectorOfInt<D>, int, D>
+    where D: IInteger, new()
+{
+    public VectorOfInt()
+    {
+    }
+
+    public VectorOfInt(params int[] values) : base(values)
+    {
+    }
+
+    public static VectorOfInt<D> operator +
+        (VectorOfInt<D> left, VectorOfInt<D> right)
+    {
+        var result = new VectorOfInt<D>();
+        var dim = new D().Value;
+        for(int i = 0; i<dim; i++)
         {
-            var adapter = new LineToPointAdapter(line);
-            adapter.ForEach(DrawPoint);
-        }
-    }
-}
-
-
-// the interface we have
-static void DrawPoint(Point p)
-{
-    Console.Write(".");
-}
-
-public class Point
-{
-    public int X;
-    public int Y;
-
-    public Point(int x, int y)
-    {
-        this.X = x;
-        this.Y = y;
-    }
-
-    protected bool Equals(Point other)
-    {
-        return X == other.X && Y == other.Y;
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Point)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        unchecked
-        {
-            return (X * 397) ^ Y;
-        }
-    }
-
-    public override string ToString()
-    {
-        return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
-    }
-}
-
-
-public class Line
-{
-    public Point Start;
-    public Point End;
-
-    public Line(Point start, Point end)
-    {
-        this.Start = start;
-        this.End = end;
-    }
-
-    protected bool Equals(Line other)
-    {
-        return Equals(Start, other.Start) && Equals(End, other.End);
-    }
-
-    public override bool Equals(object obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Line)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        unchecked
-        {
-            return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
-        }
-    }
-}
-
-public class VectorObject : Collection<Line>
-{
-
-}
-
-public class VectorRectangle : VectorObject
-{
-    public VectorRectangle(int x, int y, int width, int height)
-    {
-        Add(new Line(new Point(x, y), new Point(x + width, y)));
-        Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
-        Add(new Line(new Point(x, y), new Point(x, y + height)));
-        Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
-    }
-}
-
-public class LineToPointAdapter : IEnumerable<Point>
-{
-    private static int count = 0;
-
-    private static Dictionary<int, List<Point>> cache 
-           = new Dictionary<int, List<Point>>();
-
-    public LineToPointAdapter(Line line)
-    {
-        var hash = line.GetHashCode();
-
-        if (cache.ContainsKey(hash)) return;
-
-        Console.WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}] (no caching)");
-
-        var points = new List<Point>();
-
-        int left = Math.Min(line.Start.X, line.End.X);
-        int right = Math.Max(line.Start.X, line.End.X);
-        int top = Math.Min(line.Start.Y, line.End.Y);
-        int bottom = Math.Max(line.Start.Y, line.End.Y);
-        int dx = right - left;
-        int dy = line.End.Y - line.Start.Y;
-
-        if (dx == 0)
-        {
-            for (int y = top; y <= bottom; ++y)
-            {
-                points.Add(new Point(left, y));
-            }
-        }
-        else if (dy == 0)
-        {
-            for (int x = left; x <= right; ++x)
-            {
-                points.Add(new Point(x, top));
-            }
+            result[i] = left[i] + right[i];
         }
 
-        cache.Add(hash, points);
-    }
-
-    public IEnumerator<Point> GetEnumerator()
-    {
-        return cache.Values.SelectMany(x => x).GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
+        return result;
     }
 }
+
+public class Vector2i: VectorOfInt<Dimensions.Two>
+{
+    public Vector2i()
+    {
+    }
+
+    public Vector2i(params int[] values) : base(values)
+    {
+    }
+}
+
+public class Vector3f : VectorOfFloat<Vector3f, Dimensions.Three>
+{
+
+}
+
+public class Vector<TSelf, T, D>
+    where D : IInteger, new()
+    where TSelf : Vector<TSelf, T, D>, new()
+{
+    public T[] data;
+
+    public Vector()
+    {
+        data = new T[new D().Value];
+    }
+
+    //can't have a constructor because we do not know how many parameters it has => solution is to use params
+    public Vector(params T[] values)
+    {
+        var requiredSize = new D().Value;
+        data = new T[requiredSize];
+
+        var providedSize = values.Length;
+
+        for (int i = 0; i < Math.Min(requiredSize, providedSize); ++i)
+            data[i] = values[i];
+    }
+
+    //need a params factory method
+    public static TSelf Create(params T[] values)
+    {
+        //return new Vector<T,D>(values); -> bad approach, becasue the result of this constructor will nopt be Vector3f it will be Vector<float, Dimensions.Three>
+        //Solution is recursive generics, when you inherit from vectorfloat  which inherit from vector<float, D> you have to propagate additional info up to the base class
+
+        var result = new TSelf();
+        var requiredSize = new D().Value;
+        result.data = new T[requiredSize];
+
+        var providedSize = values.Length;
+
+        for (int i = 0; i < Math.Min(requiredSize, providedSize); ++i)
+            result.data[i] = values[i];
+
+        return result;
+    }
+
+    public T this[int index]
+    {
+        get => data[index];
+        set => data[index] = value;
+    }
+
+    public T X
+    {
+        get => data[0];
+        set => data[0] = value;
+    }
+    //the problem comes when the vector f e is a 2 dim, but it has a Z. Its dangerous to have it.
+}
+
+
+
+
