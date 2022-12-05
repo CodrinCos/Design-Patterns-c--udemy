cb7f1bfd4d7e60465f8400936412a36225a5c7f9
CodrinCos <codrin.costea22@gmail.com>
2022-12-05 07:45:22 +0100

    decorator in dependency injection

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,162 +1,46 @@
-﻿using System.Text;
-
-var circle = new Circle(2);
-var colored1 = new ColoredShape(circle, "red");
-var colored2 = new ColoredShape(colored1, "blue");
-
-Console.WriteLine(circle.AsString());
-Console.WriteLine(colored1.AsString());
-Console.WriteLine(colored2.AsString());
+﻿using Autofac;
 
 
-public abstract class Shape
+var b = new ContainerBuilder();
+
+b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+
+b.RegisterDecorator<IReportingService>((context, service) => new ReportingServiceWithLogging(service), "reporting" );
+
+using(var container = b.Build())
 {
-    public virtual string AsString() => string.Empty;
+    var r = container.Resolve<IReportingService>();
+    r.Report();
 }
 
-public sealed class Circle : Shape
+public interface IReportingService
 {
-    private float radius;
-
-    public Circle() : this(0)
-    {
-    }
-
-    public Circle(float radius)
-    {
-        this.radius = radius;
-    }
-
-    public void Resize(float factor)
-    {
-        radius *= factor;
-    }
-
-    public override string AsString() => $"A circle of radius {radius}";
+    void Report();
 }
 
-public sealed class Square : Shape
+
+public class ReportingService : IReportingService
 {
-    private readonly float side;
-
-    public Square() : this(0)
+    public void Report()
     {
-    }
-
-    public Square(float side)
-    {
-        this.side = side;
-    }
-
-    public override string AsString() => $"A square with side {side}";
-}
-
-public abstract class ShapeDecoratorCyclePolicy
-{
-    public abstract bool TypeAdditionAllowed(Type type, IList<Type> allTypes);
-    public abstract bool ApplicationAllowed(Type type, IList<Type> allTypes);
-}
-
-public class ThrowOnCyclePolicy : ShapeDecoratorCyclePolicy
-{
-    private bool handler(Type type, IList<Type> allTypes)
-    {
-        if (allTypes.Contains(type))
-            throw new InvalidOperationException(
-              $"Cycle detected! Type is already a {type.FullName}!");
-        return true;
-    }
-
-    public override bool TypeAdditionAllowed(Type type, IList<Type> allTypes)
-    {
-        return handler(type, allTypes);
-    }
-
-    public override bool ApplicationAllowed(Type type, IList<Type> allTypes)
-    {
-        return handler(type, allTypes);
+        Console.WriteLine("Here is the report");
     }
 }
 
-public class AbsorbCyclePolicy : ShapeDecoratorCyclePolicy
+
+public class ReportingServiceWithLogging : IReportingService
 {
-    public override bool TypeAdditionAllowed(Type type, IList<Type> allTypes)
+    private IReportingService decorated;
+
+    public ReportingServiceWithLogging(IReportingService decorated)
     {
-        return true;
+        this.decorated = decorated;
     }
 
-    public override bool ApplicationAllowed(Type type, IList<Type> allTypes)
+    public void Report()
     {
-        return !allTypes.Contains(type);
-    }
-}
-
-public class CyclesAllowedPolicy : ShapeDecoratorCyclePolicy
-{
-    public override bool TypeAdditionAllowed(Type type, IList<Type> allTypes)
-    {
-        return true;
-    }
-
-    public override bool ApplicationAllowed(Type type, IList<Type> allTypes)
-    {
-        return true;
-    }
-}
-
-public abstract class ShapeDecorator : Shape
-{
-    protected internal readonly List<Type> types = new();
-    protected internal Shape shape;
-
-    public ShapeDecorator(Shape shape)
-    {
-        this.shape = shape;
-        if (shape is ShapeDecorator sd)
-            types.AddRange(sd.types);
-    }
-}
-
-public abstract class ShapeDecorator<TSelf, TCyclePolicy> : ShapeDecorator
-  where TCyclePolicy : ShapeDecoratorCyclePolicy, new()
-{
-    protected readonly TCyclePolicy policy = new();
-
-    public ShapeDecorator(Shape shape) : base(shape)
-    {
-        if (policy.TypeAdditionAllowed(typeof(TSelf), types))
-            types.Add(typeof(TSelf));
-    }
-}
-
-// can determine one policy for all classes
-public class ShapeDecoratorWithPolicy<T>
-  : ShapeDecorator<T, ThrowOnCyclePolicy>
-{
-    public ShapeDecoratorWithPolicy(Shape shape) : base(shape)
-    {
-    }
-}
-
-// dynamic
-public class ColoredShape
-  : ShapeDecorator<ColoredShape, AbsorbCyclePolicy>
-{
-
-    private readonly string color;
-
-    public ColoredShape(Shape shape, string color) : base(shape)
-    {
-        this.color = color;
-    }
-
-    public override string AsString()
-    {
-        var sb = new StringBuilder($"{shape.AsString()}");
-
-        if (policy.ApplicationAllowed(types[0], types.Skip(1).ToList()))
-            sb.Append($" has the color {color}");
-
-        return sb.ToString();
+        Console.WriteLine("this is a log");
+        decorated.Report();
+        Console.WriteLine("this is the end of a log");
     }
 }
