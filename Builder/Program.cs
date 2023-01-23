a52c2403f536dba6bc062530b9034083618d7c1a
CodrinCos <codrin.costea22@gmail.com>
2023-01-23 08:09:32 +0100

    done

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -2,6 +2,8 @@
 //get an exception. I need a log which does nothing otherwise I have problems
 //var ba = new BankAccount(null);
 
+var ba = new BankAccount(new NullLog());
+
 ba.Deposit(100);
 
 
@@ -9,6 +11,25 @@ public interface ILog
 {
     void Info(string msg);
     void Warn(string msg);
+
+    public static ILog Null => NullLog.Instance;
+
+    private sealed class NullLog : ILog
+    {
+        private NullLog() { }
+
+        private static Lazy<NullLog> instance = new (() => new NullLog());
+
+        public static ILog Instance => instance.Value;
+
+        public void Info(string msg)
+        {
+        }
+
+        public void Warn(string msg)
+        {
+        }
+    }
 }
 
 class ConsoleLog : ILog
@@ -24,16 +45,32 @@ class ConsoleLog : ILog
     }
 }
 
-class NullLog : ILog
-{
-    public void Info(string msg)
-    {
-    }
+//It's not always safe becasuse sometimes you have props, or returns 
+//class NullLog : ILog
+//{
+//    public void Info(string msg)
+//    {
+//    }
 
-    public void Warn(string msg)
-    {
-    }
-}
+//    public void Warn(string msg)
+//    {
+//    }
+//}
+
+
+//Singleton
+//public sealed class NullLog : ILog
+//{
+//    private NullLog() { }
+
+//    public static Lazy<NullLog> instance;
+
+//    public static ILog Instance => instance.Value;
+
+//    public void Warn(string msg) { }
+
+//    public void Info(string msg) { }
+//}
 
 public class BankAccount
 {
