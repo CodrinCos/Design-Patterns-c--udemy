5cfbb5d0a52fe308bed351bd3f8c12ace306fbc6
CodrinCos <codrin.costea22@gmail.com>
2023-01-24 07:49:46 +0100

    done

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,91 +1,26 @@
-﻿var log = new ConsoleLog();
-//get an exception. I need a log which does nothing otherwise I have problems
-//var ba = new BankAccount(null);
+﻿var person = new Person();
 
-var ba = new BankAccount(new NullLog());
+person.FallsIll += CallDoctor;
 
-ba.Deposit(100);
+person.CatchACold();
 
-
-public interface ILog
+static void CallDoctor(object sender, FallsIllEventArgs eventArgs)
 {
-    void Info(string msg);
-    void Warn(string msg);
-
-    public static ILog Null => NullLog.Instance;
-
-    private sealed class NullLog : ILog
-    {
-        private NullLog() { }
-
-        private static Lazy<NullLog> instance = new (() => new NullLog());
-
-        public static ILog Instance => instance.Value;
-
-        public void Info(string msg)
-        {
-        }
-
-        public void Warn(string msg)
-        {
-        }
-    }
+    Console.WriteLine($"A doctor has been called to {eventArgs.Address}");
 }
 
-class ConsoleLog : ILog
+public class FallsIllEventArgs
 {
-    public void Info(string msg)
-    {
-        Console.WriteLine(msg);
-    }
-
-    public void Warn(string msg)
-    {
-        Console.WriteLine("Warning!!!" + msg);
-    }
+    public string Address;
 }
 
-//It's not always safe becasuse sometimes you have props, or returns 
-//class NullLog : ILog
-//{
-//    public void Info(string msg)
-//    {
-//    }
-
-//    public void Warn(string msg)
-//    {
-//    }
-//}
-
-
-//Singleton
-//public sealed class NullLog : ILog
-//{
-//    private NullLog() { }
-
-//    public static Lazy<NullLog> instance;
-
-//    public static ILog Instance => instance.Value;
-
-//    public void Warn(string msg) { }
-
-//    public void Info(string msg) { }
-//}
-
-public class BankAccount
+public class Person
 {
-    ILog log;
-    int balance;
-
-
-    public BankAccount(ILog log)
+    public void CatchACold()
     {
-        this.log = log;
+        FallsIll?.Invoke(this,
+          new FallsIllEventArgs { Address = "123 London Road" });
     }
 
-    public void Deposit(int amount)
-    {
-        balance += amount;
-        log.Info($"Deposited {amount}, balance is now {balance} ");
-    }
+    public event EventHandler<FallsIllEventArgs> FallsIll;
 }
