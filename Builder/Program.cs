d5c36148f25bba6802afd22d99fbf402a82d10c2
CodrinCos <codrin.costea22@gmail.com>
2023-01-27 08:09:12 +0100

    done

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,51 +1,64 @@
-﻿using System.Text;
-
-enum State
+﻿enum Chest
 {
-    Locked,
-    Failed,
-    Unlocked
+    Open,
+    Closed,
+    Locked
 }
 
-public class SwitchBasedDemo
+enum Action
 {
-    static void Main(string[] args)
+    Open,
+    Close
+}
+
+public class SwitchExpressions
+{
+    static Chest Manipulate(Chest chest,
+      Action action, bool haveKey) =>
+      (chest, action, haveKey) switch
+      {
+          (Chest.Closed, Action.Open, _) => Chest.Open,
+          (Chest.Locked, Action.Open, true) => Chest.Open,
+          (Chest.Open, Action.Close, true) => Chest.Locked,
+          (Chest.Open, Action.Close, false) => Chest.Closed,
+
+          _ => chest
+      };
+
+    static Chest Manipulate2(Chest chest,
+      Action action, bool haveKey)
     {
-        string code = "1234";
-        var state = State.Locked;
-        var entry = new StringBuilder();
-
-        while (true)
+        switch (chest, action, haveKey)
         {
-            switch (state)
-            {
-                case State.Locked:
-                    entry.Append(Console.ReadKey().KeyChar);
-
-                    if (entry.ToString() == code)
-                    {
-                        state = State.Unlocked;
-                        break;
-                    }
-
-                    if (!code.StartsWith(entry.ToString()))
-                    {
-                        // the code is blatantly wrong
-                        state = State.Failed;
-                    }
-                    break;
-                case State.Failed:
-                    Console.CursorLeft = 0;
-                    Console.WriteLine("FAILED");
-                    entry.Clear();
-                    state = State.Locked;
-                    break;
-                case State.Unlocked:
-                    Console.CursorLeft = 0;
-                    Console.WriteLine("UNLOCKED");
-                    return;
-            }
+            case (Chest.Closed, Action.Open, _):
+                return Chest.Open;
+            case (Chest.Locked, Action.Open, true):
+                return Chest.Open;
+            case (Chest.Open, Action.Close, true):
+                return Chest.Locked;
+            case (Chest.Open, Action.Close, false):
+                return Chest.Closed;
+            default:
+                Console.WriteLine("Chest unchanged");
+                return chest;
         }
     }
-}
 
+    public static void Main(string[] args)
+    {
+        Chest chest = Chest.Locked;
+        Console.WriteLine($"Chest is {chest}");
+
+        // unlock with key
+        chest = Manipulate(chest, Action.Open, true);
+        Console.WriteLine($"Chest is now {chest}");
+
+        // close it!
+        chest = Manipulate(chest, Action.Close, false);
+        Console.WriteLine($"Chest is now {chest}");
+
+        // close it again!
+        chest = Manipulate(chest, Action.Close, false);
+        Console.WriteLine($"Chest is now {chest}");
+    }
+}
