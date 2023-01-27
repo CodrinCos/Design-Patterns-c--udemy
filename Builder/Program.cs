df8987acb35dc32f47f4adcfda5dd17ad4a2052c
CodrinCos <codrin.costea22@gmail.com>
2023-01-27 09:08:39 +0100

    done

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,64 +1,84 @@
-﻿enum Chest
+﻿using System.Text;
+
+public enum OutputFormat
 {
-    Open,
-    Closed,
-    Locked
+    Markdown,
+    Html
 }
 
-enum Action
+public interface IListStrategy
 {
-    Open,
-    Close
+    void Start(StringBuilder sb);
+    void End(StringBuilder sb);
+    void AddListItem(StringBuilder sb, string item);
 }
 
-public class SwitchExpressions
+public class MarkdownListStrategy : IListStrategy
 {
-    static Chest Manipulate(Chest chest,
-      Action action, bool haveKey) =>
-      (chest, action, haveKey) switch
-      {
-          (Chest.Closed, Action.Open, _) => Chest.Open,
-          (Chest.Locked, Action.Open, true) => Chest.Open,
-          (Chest.Open, Action.Close, true) => Chest.Locked,
-          (Chest.Open, Action.Close, false) => Chest.Closed,
-
-          _ => chest
-      };
-
-    static Chest Manipulate2(Chest chest,
-      Action action, bool haveKey)
+    public void Start(StringBuilder sb)
     {
-        switch (chest, action, haveKey)
-        {
-            case (Chest.Closed, Action.Open, _):
-                return Chest.Open;
-            case (Chest.Locked, Action.Open, true):
-                return Chest.Open;
-            case (Chest.Open, Action.Close, true):
-                return Chest.Locked;
-            case (Chest.Open, Action.Close, false):
-                return Chest.Closed;
-            default:
-                Console.WriteLine("Chest unchanged");
-                return chest;
-        }
+        // markdown doesn't require a list preamble
     }
 
-    public static void Main(string[] args)
+    public void End(StringBuilder sb)
     {
-        Chest chest = Chest.Locked;
-        Console.WriteLine($"Chest is {chest}");
 
-        // unlock with key
-        chest = Manipulate(chest, Action.Open, true);
-        Console.WriteLine($"Chest is now {chest}");
+    }
 
-        // close it!
-        chest = Manipulate(chest, Action.Close, false);
-        Console.WriteLine($"Chest is now {chest}");
+    public void AddListItem(StringBuilder sb, string item)
+    {
+        sb.AppendLine($" * {item}");
+    }
+}
 
-        // close it again!
-        chest = Manipulate(chest, Action.Close, false);
-        Console.WriteLine($"Chest is now {chest}");
+public class HtmlListStrategy : IListStrategy
+{
+    public void Start(StringBuilder sb)
+    {
+        sb.AppendLine("<ul>");
+    }
+
+    public void End(StringBuilder sb)
+    {
+        sb.AppendLine("</ul>");
+    }
+
+    public void AddListItem(StringBuilder sb, string item)
+    {
+        sb.AppendLine($"  <li>{item}</li>");
+    }
+}
+
+// a.k.a. policy
+public class TextProcessor<LS> where LS : IListStrategy, new()
+{
+    private StringBuilder sb = new StringBuilder();
+    private IListStrategy listStrategy = new LS();
+
+    public void AppendList(IEnumerable<string> items)
+    {
+        listStrategy.Start(sb);
+        foreach (var item in items)
+            listStrategy.AddListItem(sb, item);
+        listStrategy.End(sb);
+    }
+
+    public override string ToString()
+    {
+        return sb.ToString();
+    }
+}
+
+class Demo
+{
+    static void Main(string[] args)
+    {
+        var tp = new TextProcessor<MarkdownListStrategy>();
+        tp.AppendList(new[] { "foo", "bar", "baz" });
+        Console.WriteLine(tp);
+
+        var tp2 = new TextProcessor<HtmlListStrategy>();
+        tp2.AppendList(new[] { "foo", "bar", "baz" });
+        Console.WriteLine(tp2);
     }
 }
